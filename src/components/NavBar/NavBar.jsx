461375c604bfdf4af5f74529793874350808a208
mohamedoa <moahmed1720@gmail.com>
2024-07-09 01:44:13 +0100

    Add navBar component along with styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,5 +1,20 @@
 import "./NavBar.scss";
+import { NavLink } from "react-router-dom";
+import logoIcon from "../../assets/images/mynewleaf.png";
 
 export default function NavBar() {
-  return <></>;
+  return (
+    <nav className="nav">
+      <div className="logo">
+        <NavLink to="/" className="logo__link">
+          <img className="logo__image" src={logoIcon} alt="" />
+        </NavLink>
+      </div>
+      <div className="nav__item">
+        <NavLink to="/login" className="nav__link nav__cta">
+          Login
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
