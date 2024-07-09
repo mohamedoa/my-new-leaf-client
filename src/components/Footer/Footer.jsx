96cd155ea6d5de4905e883fbd1f78092b812ecf9
mohamedoa <moahmed1720@gmail.com>
2024-07-09 01:50:14 +0100

    Add footer component and styling, corrected sass mistake in navbar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,31 @@
 import "./Footer.scss";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className="footer">
+      <ul className="footer__list">
+        <li className="footer__item">
+          <Link to="/" className="footer__link">
+            Home
+          </Link>
+        </li>
+        <li className="footer__item">
+          <Link to="/habits" className="footer__link">
+            My Garden
+          </Link>
+        </li>
+        <li className="footer__item">
+          <Link to="/community" className="footer__link">
+            Community
+          </Link>
+        </li>
+        <li className="footer__item">
+          <Link to="/users/1" className="footer__link">
+            Profile
+          </Link>
+        </li>
+      </ul>
+    </footer>
+  );
 }
