44131edfb673722c902b3146932e475adadfa2fb
mohamedoa <moahmed1720@gmail.com>
2024-07-09 01:37:51 +0100

    Add routes for each page and portal in index.html for modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 import "./App.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
+import Home from "./pages/Home/Home";
+import Garden from "./pages/Garden/Garden";
+import Community from "./pages/Community/Community";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Profile from "./pages/Profile/Profile";
+import HabitTree from "./pages/HabitTree/HabitTree";
 
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/habits/:id" Component={HabitTree} />
+          <Route path="/habits" Component={Garden} />
+          <Route path="/community" Component={Community} />
+          <Route path="/users/:id" Component={Profile} />
+          <Route path="/login" Component={Login} />
+          <Route path="/register" Component={Register} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
