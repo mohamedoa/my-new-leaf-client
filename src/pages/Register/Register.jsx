c91bc1d314a9253c92232c0a12bff3b20bbecf0e
mohamedoa <moahmed1720@gmail.com>
2024-07-09 03:13:52 +0100

    Add register page and styling

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,5 +1,54 @@
 import "./Register.scss";
+import { Link } from "react-router-dom";
 
 export default function Register() {
-  return <></>;
+  const handleRegister = () => {
+    console.log("Register");
+  };
+
+  return (
+    <main className="register">
+      <article className="register__container">
+        <h1 className="register__header">Register</h1>
+        <form className="form" onSubmit={handleRegister}>
+          <label className="label" htmlFor="register-username">
+            Username
+          </label>
+          <input
+            id="register-username"
+            name="register-username"
+            className="input"
+            type="text"
+            placeholder="JaneDoe"
+          />
+
+          <label className="label" htmlFor="register-password">
+            Password
+          </label>
+          <input
+            id="register-password"
+            name="register-password"
+            className="input"
+            type="password"
+            placeholder="Password"
+          />
+
+          <label className="label" htmlFor="register-confirm">
+            Confirm Password
+          </label>
+          <input
+            id="register-confirm"
+            name="register-confirm"
+            className="input"
+            type="password"
+            placeholder="Password"
+          />
+          <button className="register__button">REGISTER</button>
+        </form>
+        <Link className="register__link" to="/login">
+          or Login
+        </Link>
+      </article>
+    </main>
+  );
 }
