6721d96a5c2aa12f7e8fea4695d0047d055da3c9
mohamedoa <moahmed1720@gmail.com>
2024-07-09 03:59:15 +0100

    Add delete/create modals for habit rendering on portals

--- src/components/CreateHabitModal/CreateHabitModal.jsx
+++ src/components/CreateHabitModal/CreateHabitModal.jsx
@@ -1,5 +1,68 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import "./CreateHabitModal.scss";
+import { useState } from "react";
+import axios from "axios";
+import closeIcon from "../../assets/images/icons/close.svg";
 
-export default function CreateHabitModal() {
-  return <></>;
+const API_URL = import.meta.env.VITE_API_URL;
+
+export default function CreateHabitModal({ open, onClose, fetchHabits }) {
+  const [habitName, setHabitName] = useState("");
+
+  if (!open) {
+    return null;
+  }
+
+  const handleUpload = async (event) => {
+    try {
+      event.preventDefault();
+      const { data } = await axios.post(`${API_URL}/api/habits`, {
+        user_id: 1,
+        habit_name: habitName,
+      });
+      fetchHabits();
+      onClose();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return ReactDOM.createPortal(
+    <>
+      <div className="overlay" />
+      <article className="modal">
+        <div className="modal__container">
+          <button className="modal__close" onClick={onClose}>
+            <img className="modal__close-image" src={closeIcon} alt="" />
+          </button>
+          <h1 className="modal__header">New Habit</h1>
+          <form onSubmit={handleUpload} className="modal__body">
+            <label htmlFor="item_name">Habit Name</label>
+            <input
+              type="text"
+              className="ea-form__input"
+              id="item_name"
+              name="item_name"
+              placeholder="Habit Name"
+              value={habitName}
+              onChange={(event) => setHabitName(event.target.value)}
+            />
+            <button className="modal__button  modal__button--delete">
+              Create Habit
+            </button>
+          </form>
+        </div>
+        <div className="modal__button-wrapper">
+          <button
+            className="modal__button modal__button--cancel"
+            onClick={onClose}
+          >
+            Cancel
+          </button>
+        </div>
+      </article>
+    </>,
+    document.getElementById("portal")
+  );
 }
--- src/components/DeleteHabitModal/DeleteHabitModal.jsx
+++ src/components/DeleteHabitModal/DeleteHabitModal.jsx
@@ -1,5 +1,72 @@
+import React from "react";
+import ReactDOM from "react-dom";
 import "./DeleteHabitModal.scss";
+import { useState } from "react";
+import axios from "axios";
+import closeIcon from "../../assets/images/icons/close.svg";
 
-export default function DeleteHabitModal() {
-  return <></>;
+const API_URL = import.meta.env.VITE_API_URL;
+
+export default function DeleteHabitModal({
+  deleteOpen,
+  onClose,
+  fetchHabits,
+  habits,
+  id,
+}) {
+  const [habit, setHabit] = useState(null);
+
+  if (!deleteOpen) {
+    return null;
+  }
+
+  const handleDelete = async () => {
+    try {
+      const { data } = await axios.delete(`${API_URL}/api/habits/${id}`);
+      fetchHabits();
+      setHabit(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const findHabit = habits.find((habit) => habit.id === id);
+
+  const habitName = findHabit.habit_name;
+
+  return ReactDOM.createPortal(
+    <>
+      <div className="overlay" />
+      <article className="modal">
+        <div className="modal__container">
+          <button className="modal__close" onClick={onClose}>
+            <img className="modal__close-image" src={closeIcon} alt="" />
+          </button>
+          <h1 className="modal__header">Delete {habitName}?</h1>
+          <p className="modal__body">
+            Please confirm that you’d like to delete the {habitName} tree from
+            your garden. You won’t be able to undo this action.
+          </p>
+        </div>
+        <div className="modal__button-wrapper">
+          <button
+            className="modal__button  modal__button--delete"
+            onClick={() => {
+              handleDelete();
+              onClose();
+            }}
+          >
+            Delete
+          </button>
+          <button
+            className="modal__button modal__button--cancel"
+            onClick={onClose}
+          >
+            Cancel
+          </button>
+        </div>
+      </article>
+    </>,
+    document.getElementById("portal")
+  );
 }
